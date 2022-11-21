b9d0fbb655f9099567892e3801a0a9e0eea1dda6
matudinglin <martin_lin@mail.dlut.edu.cn>
2022-11-21 12:48:18 -0800

    Added Phong Shading

--- assign3/assign3/assign3.cpp
+++ assign3/assign3/assign3.cpp
@@ -12,6 +12,7 @@ Name: Yao Lin
 
 #include <stdio.h>
 #include <string>
+#include <iostream>
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/core/utility.hpp"
@@ -37,8 +38,14 @@ int mode = MODE_DISPLAY;
 //the field of view of the camera
 #define FOV 60.0
 const double PI = 3.1415926;
+const double FURTHEST = -DBL_MAX;
+const double ZERO = DBL_MIN;
 
 unsigned char buffer[HEIGHT][WIDTH][3];
+
+using std::cout;
+using std::endl;
+using std::clog;
 
 // OpenCV like Vec3d
 struct Vec3d
@@ -67,12 +74,24 @@ struct Vec3d
 		return *this;
 	}
 	Vec3d operator*(double c) const { return Vec3d(c * x, c * y, c * z); }
+	friend Vec3d operator*(double c, const Vec3d& v)
+	{
+		return Vec3d(c * v.x, c * v.y, c * v.z);
+	}
 	Vec3d& operator*=(double c)
 	{
 		x *= c;
 		y *= c;
 		z *= c;
 		return *this;
+	}
+	Vec3d& operator*(const Vec3d& v2) const
+	{
+		Vec3d v;
+		v.x = x * v2.x;
+		v.y = y * v2.y;
+		v.z = z * v2.z;
+		return v;
 	}
 	Vec3d operator/(double c) const { return Vec3d(x / c, y / c, z / c); }
 	Vec3d& operator/=(double c)
@@ -94,6 +113,14 @@ struct Vec3d
 		*this /= length();
 		return *this;
 	}
+
+	Vec3d& clamped()
+	{
+		if (x > 1.0) x = 1.0; else if (x < 0.0) x = 0.0;
+		if (y > 1.0) y = 1.0; else if (y < 0.0) y = 0.0;
+		if (z > 1.0) z = 1.0; else if (z < 0.0) z = 0.0;
+		return *this;
+	}
 };
 double dot(const Vec3d& v1, const Vec3d& v2){ return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
 Vec3d cross(const Vec3d& v1, const Vec3d& v2)
@@ -103,6 +130,11 @@ Vec3d cross(const Vec3d& v1, const Vec3d& v2)
 	v.y = v1.z * v2.x - v1.x * v2.z;
 	v.z = v1.x * v2.y - v1.y * v2.x;
 	return v;
+}
+void clamp(double& val)
+{
+	if (val < 0.0) val = 0.0;
+	else if (val > 1.0) val = 1.0;
 }
 
 struct Vertex
@@ -143,7 +175,7 @@ struct Ray
 	Ray() = default;
 	Ray(const Vec3d& _pos, const Vec3d& _dir) : pos(_pos), dir(_dir) {};
 	// check ray & sphere intersection
-	bool getHitPosition(Vec3d &hit, const Sphere& s)
+	bool getHitPosition(const Sphere& s, Vec3d &hitPos) const
 	{
 		double a = 1;
 		double b = 2 * (dir.x * (pos.x - s.position[0]) + 
@@ -153,43 +185,49 @@ struct Ray
 				   pow(pos.y - s.position[1], 2) + 
 				   pow(pos.z - s.position[2], 2) - 
 			       pow(s.radius, 2);
-		double t1 = (-b + sqrt(b * b - 4 * a * c)) / 2;
-		double t2 = (-b - sqrt(b * b - 4 * a * c)) / 2;
+		double delta = b * b - 4.0 * a * c;
 
-		// no hit
+		// no hitPos
+		if (delta <= ZERO)  return false; 
+		double t1 = (-b + sqrt(delta)) / 2;
+		double t2 = (-b - sqrt(delta)) / 2;
+		// backward ray
 		if (t1 <= 0 && t2 <= 0)
 		{
 			return false;
 		}
 
-		// hit
+		// hitPos
 		double hitTime;
 		if (t1 > t2 && t2 > 0) 
 			hitTime = t2;
 		else 
 			hitTime = t1;
-		hit = pos + dir * hitTime;
+		hitPos = pos + dir * hitTime;
 		return true;
 	}
 	// check ray & triangle intersection
-	bool getHitPosition(Vec3d &hit, const Triangle& t)
+	bool getHitPosition(const Triangle& t, Vec3d &hitPos) const
 	{
 		// Normal for ray-plane intersection = (B-A)x(C-A) (and you must normalize this vector)
 		Vec3d A(t.v[0].position[0], t.v[0].position[1], t.v[0].position[2]);
 		Vec3d B(t.v[1].position[0], t.v[1].position[1], t.v[1].position[2]);
 		Vec3d C(t.v[2].position[0], t.v[2].position[1], t.v[2].position[2]);
-		Vec3d N = cross(B - A, C - A); 
-		N.normalized();
-		double hitTime = dot(A - pos, N) / dot(dir, N);
+		Vec3d N = cross(B - A, C - A).normalized(); 
+		
+		// no intersection
+		if (abs(dot(N, dir)) <= ZERO) return false;
+		// compute hit point time
+		double hitTime = (dot(N, A) - dot(N, pos)) / dot(N, dir);
 
-		// no hit the plane
+		// hitPos is not on the plane
 		if (hitTime <= 0) return false;
 
-		// hit the plane
-		hit = pos + dir * hitTime;
-		if (dot(cross(B - A, hit - A), N) < 0 ||
-			dot(cross(B - A, hit - A), N) < 0 ||
-			dot(cross(B - A, hit - A), N) < 0) return false;
+		// hitPos is on the plane
+		hitPos = pos + dir * hitTime;
+		if (dot(cross(B - A, hitPos - A), N) < 0 ||
+			dot(cross(A - C, hitPos - C), N) < 0 ||
+			dot(cross(C - B, hitPos - B), N) < 0) return false;
 		return true;
 	}
 };
@@ -208,9 +246,169 @@ void plot_pixel_display(int x, int y, unsigned char r, unsigned char g, unsigned
 void plot_pixel_jpeg(int x, int y, unsigned char r, unsigned char g, unsigned char b);
 void plot_pixel(int x, int y, unsigned char r, unsigned char g, unsigned char b);
 
-Vec3d computeColor(const Ray &ray, )
+Vec3d PhongShading(const Sphere& s, Vec3d& hitPos, const Light& l)
+{
+	Vec3d lightPos(l.position[0], l.position[1], l.position[2]);
+	Vec3d lightColor(l.color[0], l.color[1], l.color[2]);
+	// diffuse
+	Vec3d spherePos(s.position[0], s.position[1], s.position[2]);
+	Vec3d L = (lightPos - hitPos).normalized();
+	Vec3d N = (hitPos - spherePos).normalized();
+	double LdotN = dot(L, N); 
+	clamp(LdotN);
+	Vec3d kd(s.color_diffuse[0], s.color_diffuse[1], s.color_diffuse[2]);
+	// specular
+	Vec3d R = (N * (2.0 * (LdotN)) - L).normalized(); 
+	Vec3d V = (-hitPos).normalized();  // camera is on 0.0, 0.0, 0.0
+	double RdotV = dot(R, V);
+	clamp(RdotV);
+	Vec3d ks(s.color_specular[0], s.color_specular[1], s.color_specular[2]);
+	double sep_exp = s.shininess;
+	// ambient is omitted since it cannot be added multiple times
+	// Phong shading
+	return lightColor * kd * LdotN + ks * pow(RdotV, sep_exp);
+}
+
+Vec3d PhongShading(const Triangle& t, Vec3d& hitPos, const Light& l)
+{
+	Vec3d A_N(t.v[0].normal[0], t.v[0].normal[1], t.v[0].normal[2]);
+	Vec3d B_N(t.v[1].normal[0], t.v[1].normal[1], t.v[1].normal[2]);
+	Vec3d C_N(t.v[2].normal[0], t.v[2].normal[1], t.v[2].normal[2]);
+	Vec3d A_D(t.v[0].color_diffuse[0], t.v[0].color_diffuse[1], t.v[0].color_diffuse[2]);
+	Vec3d B_D(t.v[1].color_diffuse[0], t.v[1].color_diffuse[1], t.v[1].color_diffuse[2]);
+	Vec3d C_D(t.v[2].color_diffuse[0], t.v[2].color_diffuse[1], t.v[2].color_diffuse[2]);
+	Vec3d A_S(t.v[0].color_specular[0], t.v[0].color_specular[1], t.v[0].color_specular[2]);
+	Vec3d B_S(t.v[1].color_specular[0], t.v[1].color_specular[1], t.v[1].color_specular[2]);
+	Vec3d C_S(t.v[2].color_specular[0], t.v[2].color_specular[1], t.v[2].color_specular[2]);
+
+	Vec3d lightPos(l.position[0], l.position[1], l.position[2]);
+	Vec3d lightColor(l.color[0], l.color[1], l.color[2]);
+	// use barycentric coordinates to interpolate normal
+	Vec3d A(t.v[0].position[0], t.v[0].position[1], t.v[0].position[2]);
+	Vec3d B(t.v[1].position[0], t.v[1].position[1], t.v[1].position[2]);
+	Vec3d C(t.v[2].position[0], t.v[2].position[1], t.v[2].position[2]);
+	Vec3d P = hitPos;
+	double alpha = cross(P - C, P - B).length() / cross(B - A, C - A).length();
+	double beta = cross(P - A, P - C).length() / cross(B - A, C - A).length();
+	double gamma = 1 - alpha - beta;
+	// diffuse
+	Vec3d L = (lightPos - hitPos).normalized();
+	Vec3d N = alpha * A_N + beta * B_N + gamma * C_N;
+	double LdotN = dot(L, N);
+	clamp(LdotN);
+	Vec3d kd = alpha * A_D + beta * B_D + gamma * C_D;
+	// specular
+	Vec3d R = (N * (2.0 * (LdotN)) - L).normalized();
+	Vec3d V = (-hitPos).normalized(); 
+	double RdotV = dot(R, V);
+	clamp(RdotV);
+	Vec3d ks = alpha * A_S + beta * B_S + gamma * C_S;
+	double sep_exp = alpha * t.v[0].shininess + beta * t.v[1].shininess + gamma * t.v[2].shininess;
+	// Phong shading
+	return lightColor * kd * LdotN + ks * pow(RdotV, sep_exp);
+}
+
+Vec3d computeSpheresIntersection(const Ray& ray, Vec3d& color, bool &hitAnObject, Vec3d &hitPos)
 {
 
+	for (int i = 0; i < num_spheres; ++i)
+	{
+		Vec3d newHitPos;
+		if (ray.getHitPosition(spheres[i], newHitPos) && newHitPos.z > hitPos.z)
+		{
+			//clog << "Hit Sphere: " << i << endl;
+			//color = Vec3d();
+			for (int j = 0; j < num_lights; ++j)
+			{
+				//// determine if the hit point is in shadow
+				//Vec3d lightPos(lights[j].position[0], lights[j].position[1], lights[j].position[2]);
+				//Vec3d shadowRayDir = (lightPos - hitPos).normalized();
+				//Ray shadowRay(hitPos, shadowRayDir);
+				//bool inShadow = false;
+				//for (int k = 0; k < num_spheres; ++k)
+				//{
+				//	if (shadowRay.getHitPosition(spheres[k], Vec3d()))
+				//	{
+				//		inShadow = true;
+				//		break;
+				//	}
+				//}
+				//for (int k = 0; k < num_triangles; ++k)
+				//{
+				//	if (shadowRay.getHitPosition(triangles[k], Vec3d()))
+				//	{
+				//		inShadow = true;
+				//		break;
+				//	}
+				//}
+				//// if not, use Phong shading
+				//if (!inShadow)
+					color = PhongShading(spheres[i], newHitPos, lights[j]);
+			}
+			hitAnObject = true;
+			hitPos = newHitPos;
+		}
+
+	}
+	return color;
+}
+
+Vec3d computeTrianglesIntersection(const Ray& ray, Vec3d& color, bool &hitAnObject, Vec3d& hitPos)
+{
+	for (int i = 0; i < num_triangles; ++i)
+	{
+		Vec3d newHitPos;
+		if (ray.getHitPosition(triangles[i], newHitPos) && newHitPos.z > hitPos.z)
+		{
+			//clog << "Hit Triangles: " << i << endl;
+			//color = Vec3d();
+			for (int j = 0; j < num_lights; ++j)
+			{
+				//// determine if the hit point is in shadow
+				//Vec3d lightPos(lights[j].position[0], lights[j].position[1], lights[j].position[2]);
+				//Vec3d shadowRayDir = (lightPos - hitPos).normalized();
+				//Ray shadowRay(hitPos, shadowRayDir);
+				//bool inShadow = false;
+				//for (int k = 0; k < num_spheres; ++k)
+				//{
+				//	if (shadowRay.getHitPosition(spheres[k], Vec3d()))
+				//	{
+				//		inShadow = true;
+				//		break;
+				//	}
+				//}
+				//for (int k = 0; k < num_triangles; ++k)
+				//{
+				//	if (shadowRay.getHitPosition(triangles[k], Vec3d()))
+				//	{
+				//		inShadow = true;
+				//		break;
+				//	}
+				//}
+				//// if not, use Phong shading
+				//if (!inShadow)
+					color = PhongShading(triangles[i], newHitPos, lights[j]);
+			}
+			hitAnObject = true;
+			hitPos = newHitPos;
+		}
+	}
+	return color;
+}
+
+Vec3d computeColor(const Ray &ray)
+{
+	Vec3d color(0.0, 0.0, 0.0);
+	bool hitSphere = false, hitTriangle = false;
+	Vec3d hitPos = Vec3d(0.0, 0.0, FURTHEST);
+	computeSpheresIntersection(ray, color, hitSphere, hitPos);
+	computeTrianglesIntersection(ray, color, hitTriangle, hitPos);
+
+
+	if (!hitSphere && !hitTriangle)
+		return Vec3d(1.0, 1.0, 1.0);
+	else
+		return color;
 }
 
 Ray generateRay(double x, double y)
@@ -228,9 +426,9 @@ Ray generateRay(double x, double y)
 	x = x * fov * aspectRatio;
 	y = y * fov;
 	// generate ray
-	Vec3d pos(0.0, 0.0, -1.0);
-	Vec3d xy(x, y, 0.0);
-	Vec3d dir = xy - pos;
+	Vec3d pos(0.0, 0.0, 0.0); // camera is on 0.0, 0.0, 0.0
+	Vec3d xy(x, y, -1.0); // pixel is on x, y, -1.0
+	Vec3d dir = (xy - pos).normalized();
 	return Ray(pos, dir);
 }
 
@@ -243,7 +441,10 @@ void draw_scene()
 		glBegin(GL_POINTS);
 		for (y = 0; y < HEIGHT; y++)
 		{
-			plot_pixel(x, y, x % 256, y % 256, (x + y) % 256);
+			Vec3d color = computeColor(generateRay(x, y));
+			color += Vec3d(ambient_light[0], ambient_light[1], ambient_light[2]);
+			color.clamped();
+			plot_pixel(x, y, color.x * 255, color.y * 255, color.z * 255);
 		}
 		glEnd();
 		glFlush();
